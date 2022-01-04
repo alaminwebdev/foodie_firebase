c1f06ec9f2943de17af7e3a62fcba4c2d8121c68
Md Al Amin <alamiin.bd@gmail.com>
2022-01-05 00:10:36 +0600

    add loading into home when image are still loading

--- src/component/body/home/Home.js
+++ src/component/body/home/Home.js
@@ -1,104 +1,117 @@
-import React, { useState } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { fetchDishes } from "../../../redux/actionCreators";
 
-import DISHES from '../../../data/dishes';
-import SlideText from './SlideText';
-import SlideImage from './SlideImage';
+import DISHES from "../../../data/dishes";
+import SlideText from "./SlideText";
+import SlideImage from "./SlideImage";
+import Loading from "../Loading";
 
-import { motion, AnimatePresence } from 'framer-motion';
-import { wrap } from '@popmotion/popcorn';
+import { motion, AnimatePresence } from "framer-motion";
+import { wrap } from "@popmotion/popcorn";
 
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box'
-import Grid from '@mui/material/Grid';
-import Fab from '@mui/material/Fab';
-import ArrowLeftRoundedIcon from '@mui/icons-material/ArrowLeftRounded';
-import ArrowRightRoundedIcon from '@mui/icons-material/ArrowRightRounded';
-
-
-
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Fab from "@mui/material/Fab";
+import ArrowLeftRoundedIcon from "@mui/icons-material/ArrowLeftRounded";
+import ArrowRightRoundedIcon from "@mui/icons-material/ArrowRightRounded";
+import { useEffect } from "react";
 
 //mui css style start
 const slideButtonStyle = {
-    color: '#fff',
-    bgcolor: '#007FFF',
-    "&:hover": { bgcolor: '#007FFF' }
-}
-
+    color: "#fff",
+    bgcolor: "#007FFF",
+    "&:hover": { bgcolor: "#007FFF" },
+};
 
 //mui css style end
 
+const mapStateToProps = (state) => {
+    return {
+        item: state.dishes.dishes,
+    };
+};
+const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchDishes: () => dispatch(fetchDishes()),
+    };
+};
 
-const Home = () => {
-
+const Home = (props) => {
     const [[page, direction], setPage] = useState([0, 0]);
     //const imageIndex = wrap(0, images.length, page);
     //const itemIndex = wrap(0, item.length, page);
+
     const Dishes = wrap(0, DISHES.length, page);
 
-
     //console.log(DISHES[Dishes].name);
-    const paginate = newDirection => {
+    const paginate = (newDirection) => {
         setPage([page + newDirection, newDirection]);
-        //this Dishes will print index number of DISHES object 
+        //this Dishes will print index number of DISHES object
         //console.log(Dishes);
     };
 
-    return (
-        <Container maxWidth='xl'
-            className="slide-container"
-            sx={{
-                // 1 means 8px 
-                alignItems: 'center',
-                py: 10,
-                position: 'relative'
-            }}
-        >
-            <AnimatePresence initial='initial'  >  {/* AnimatePresence this tag for framer motion */}
-                <motion.div key={DISHES[Dishes]} >
-                    <Grid container spacing={2} sx={{ alignItems: 'center' }}>
-                        <Grid item xs={12} sm={12} md={6} lg={7}>
-                            <SlideText
-                                dishIndex={DISHES[Dishes]}
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={12} md={6} lg={5}>
-                            <SlideImage
-                                src={DISHES[Dishes]}
-                                custom={direction}
-                                paginate={paginate}
-                            />
-                        </Grid>
-                    </Grid>
-                </motion.div>
-            </AnimatePresence>
-            <Box
+    //Runs only on the first render
+    useEffect(() => {
+        props.fetchDishes();
+    }, []);
+
+    if (props.item.length === 0) {
+        return (
+            <Container>
+                <Grid container spacing={2} sx={{ my: 5 }}>
+                    <Loading />
+                </Grid>
+            </Container>
+        );
+    } else {
+        //console.log(props.item[Dishes]);
+        return (
+            <Container
+                maxWidth="xl"
+                className="slide-container"
                 sx={{
-                    position: 'absolute',
-                    left: '50%',
-                    bottom: '0',
-                    transform: 'translateX(-50%)'
+                    // 1 means 8px
+                    alignItems: "center",
+                    py: 10,
+                    position: "relative",
                 }}
             >
-                <Fab
-                    sx={{ ...slideButtonStyle, mr: 1 }}
-                    size="small"
-                    aria-label="previous"
-                    onClick={() => paginate(-1)}
+                <AnimatePresence initial="initial">
+                    {" "}
+                    {/* AnimatePresence this tag for framer motion */}
+                    <motion.div key={props.item[Dishes]}>
+                        <Grid container spacing={2} sx={{ alignItems: "center" }}>
+                            <Grid item xs={12} sm={12} md={6} lg={7}>
+                                <SlideText dishIndex={props.item[Dishes]} />
+                            </Grid>
+                            <Grid item xs={12} sm={12} md={6} lg={5}>
+                                <SlideImage src={props.item[Dishes]} custom={direction} paginate={paginate} />
+                            </Grid>
+                        </Grid>
+                    </motion.div>
+                </AnimatePresence>
+                <Box
+                    sx={{
+                        position: "absolute",
+                        left: "50%",
+                        bottom: "0",
+                        transform: "translateX(-50%)",
+                    }}
                 >
-                    <ArrowLeftRoundedIcon />
-                </Fab>
+                    <Fab sx={{ ...slideButtonStyle, mr: 1 }} size="small" aria-label="previous" onClick={() => paginate(-1)}>
+                        <ArrowLeftRoundedIcon />
+                    </Fab>
 
-                <Fab
-                    sx={{ ...slideButtonStyle }}
-                    size="small"
-                    aria-label="next"
-                    onClick={() => paginate(1)}
-                >
-                    <ArrowRightRoundedIcon />
-                </Fab>
-            </Box>
-        </Container>
-    )
-}
+                    <Fab sx={{ ...slideButtonStyle }} size="small" aria-label="next" onClick={() => paginate(1)}>
+                        <ArrowRightRoundedIcon />
+                    </Fab>
+                </Box>
+            </Container>
+        );
+    }
+};
 
-export default Home
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- src/component/body/home/SlideImage.js
+++ src/component/body/home/SlideImage.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import { motion } from 'framer-motion';
-import { Box } from '@mui/system';
-
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import { Box } from "@mui/system";
+import loadingGif from "./loading.gif";
 
 //framer motion effect start
 const variants = {
     enter: (direction) => {
         return {
             x: direction > 0 ? 1000 : -500,
-            opacity: 0
+            opacity: 0,
         };
     },
     center: {
         zIndex: 1,
         x: 0,
-        opacity: 1
+        opacity: 1,
     },
     exit: (direction) => {
         return {
             zIndex: 0,
             x: direction < 0 ? 1000 : -500,
-            opacity: 0
+            opacity: 0,
         };
-    }
+    },
 };
 
 const swipeConfidenceThreshold = 10000;
@@ -31,19 +31,23 @@ const swipePower = (offset, velocity) => {
 };
 //framer motion effect end
 
+const SlideImage = (props) => {
+    //const [imgLoading, isLoading] = useState(loadingGif);
+    const [img, setImg] = useState(loadingGif);
+    const paginate = props.paginate;
+    const direction = props.custom;
 
-const SlideImage = props => {
-
-    const paginate = props.paginate ;
-    const direction = props.custom ;
-
+    const checkImg = () => {
+        setImg(props.src.image);
+    };
     return (
-        <Box >
+        <Box>
             <motion.img
                 className="itemImage"
                 key={props.src.id}
-                width='100%'
-                src={props.src.image}
+                width="100%"
+                onLoad={checkImg}
+                src={img}
                 custom={direction}
                 variants={variants}
                 initial="enter"
@@ -51,7 +55,7 @@ const SlideImage = props => {
                 exit="exit"
                 transition={{
                     x: { type: "spring", mass: 0.5 },
-                    opacity: { duration: 0.8 }
+                    opacity: { duration: 0.8 },
                 }}
                 whileHover={{ scale: 1.05 }}
                 drag="x"
@@ -66,10 +70,9 @@ const SlideImage = props => {
                         paginate(-1);
                     }
                 }}
-
             />
         </Box>
-    )
-}
+    );
+};
 
-export default SlideImage
+export default SlideImage;
